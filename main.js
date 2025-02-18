56692f032a81f26cf89e40cd54a3d78bd914e198
“alerenna” <alessandrorennafm@gmail.com>
2025-02-18 23:36:22 +0800

    Add bonus solution with condition in the function for make the image and the text on the button change on every click

--- main.js
+++ main.js
@@ -6,9 +6,20 @@ console.log(imageEl);
 
 
 
-buttonEl.addEventListener('click', function () {
+/* buttonEl.addEventListener('click', function () {
     imageEl.src = './assets/img/yellow_lamp.png'
-})
+}) */
 
+buttonEl.addEventListener('click', function () {
+    
+    if (buttonEl.innerText.includes('Accendi')) {
+        imageEl.src = './assets/img/yellow_lamp.png'
+        buttonEl.innerText = 'Spegni'
+    } else {
+        imageEl.src = './assets/img/white_lamp.png'
+        buttonEl.innerText = 'Accendi'
+    }
+    
+})
 
 
